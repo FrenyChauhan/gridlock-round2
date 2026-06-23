6a3acc2872251894bc93baf5cd94a0448334e1b2
Siddh_shah <shahsiddhp@gmail.com>
2026-06-23 12:33:56 +0530

    Fix: Switch to HashRouter to bypass Render rewrite rule requirement

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from './contexts/AuthContext';
@@ -20,7 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
           <Toaster 
             position="bottom-right"
@@ -34,7 +34,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
               }
             }}
           />
-        </BrowserRouter>
+        </HashRouter>
       </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
